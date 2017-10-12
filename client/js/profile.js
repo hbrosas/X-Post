9b5152b8e7510a87236f7db4c6ed67e8bf4c1d7b
sygted <eugene_lim@dlsu.edu.ph>
2017-10-13 04:57:53 +0800

    profile and tweaked other pages

--- client/js/profile.js
+++ client/js/profile.js
@@ -1,27 +1,48 @@
-$(document).ready(function() {
-$(".btn-pref .btn").click(function () {
-    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-    // $(".tab").addClass("active"); // instead of this do the below
-    $(this).removeClass("btn-default").addClass("btn-primary");
-});
+// $(document).ready(function() {
+// $(".btn-pref .btn").click(function () {
+//     $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+//     // $(".tab").addClass("active"); // instead of this do the below
+//     $(this).removeClass("btn-default").addClass("btn-primary");
+// });
+// });
+//
+// var freedomTab = $("#freedomTab");
+// var questionTab = $("questionTab");
+//
+// freedomTab.hide();
+//
+//
+// $(document).ready(function(){
+// $("freedomLbl").click(function(){
+//   questionTab.hide();
+//   freedomTab.show();
+// });
+// });
+//
+// $(document).ready(function(){
+// $("#questionLbl").click(function(){
+//   questionTab.show();
+//   freedomTab.hide();
+// });
+// });
+
+var sendMessage = $("#sendMessageForm");
+var sendQuestion = $("#sendQuestionForm");
+
+sendMessage.hide();
+sendQuestion.hide();
+
+function resetForm() {
+  sendMessage.hide();
+  sendQuestion.hide();
+};
+
+$(document).on("click", "#messageBtn", function(){
+  resetForm();
+  sendMessage.show();
 });
 
-var freedomTab = $("#freedomTab");
-var questionTab = $("questionTab");
-
-freedomTab.hide();
-
-
-$(document).ready(function(){
-$("freedomLbl").click(function(){
-  questionTab.hide();
-  freedomTab.show();
-});
-});
-
-$(document).ready(function(){
-$("#questionLbl").click(function(){
-  questionTab.show();
-  freedomTab.hide();
-});
+$(document).on("click", "#askBtn", function(){
+  resetForm();
+  sendQuestion.show();
 });
