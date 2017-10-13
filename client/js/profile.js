7ce4e7e393a6b8c8a09f67578aa0f72bfb7b76d4
sygted <eugene_lim@dlsu.edu.ph>
2017-10-13 12:30:27 +0800

    she

--- client/js/profile.js
+++ client/js/profile.js
@@ -1,30 +1,6 @@
-// $(document).ready(function() {
-// $(".btn-pref .btn").click(function () {
-//     $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-//     // $(".tab").addClass("active"); // instead of this do the below
-//     $(this).removeClass("btn-default").addClass("btn-primary");
-// });
-// });
-//
-// var freedomTab = $("#freedomTab");
-// var questionTab = $("questionTab");
-//
-// freedomTab.hide();
-//
-//
-// $(document).ready(function(){
-// $("freedomLbl").click(function(){
-//   questionTab.hide();
-//   freedomTab.show();
-// });
-// });
-//
-// $(document).ready(function(){
-// $("#questionLbl").click(function(){
-//   questionTab.show();
-//   freedomTab.hide();
-// });
-// });
+
+
+
 
 var sendMessage = $("#sendMessageForm");
 var sendQuestion = $("#sendQuestionForm");
